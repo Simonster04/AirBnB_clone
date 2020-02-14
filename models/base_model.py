f84d4f2fdee7fb603a9044d7f04ab3da65d80c3b
Simonster04 <simon.pahe@gmail.com>
2020-02-14 16:58:58 -0500

    fixing task 4

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,22 @@ from datetime import datetime
 class BaseModel():
     """ defines all common attributes/methods for other classes """
 
-    def __init__(self, id=0, created_at=0, updated_at=0):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        self.key = datetime.strptime(val, '%m/%d/%y %H:%M:%S')
+
+                    self.key = val
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ prints as string """
-        return "{} {} {}".format([self.__class__.__name__], (self.id), self.__dict__)
+        return "{} ({}) {}".format([self.__class__.__name__], (self.id), self.__dict__)
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime """
@@ -24,9 +32,9 @@ class BaseModel():
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance """
-        aux = {}
+
         for key, val in (self.__dict__).items():
             if key == 'created_at' or key == 'updated_at':
-                aux[key] = val.strftime('%Y-%m-%dT%H:%M:%S.%f')
-            aux[key] = val
-        return aux
+                self.__dict__[key] = val.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            self.__dict__[key] = val
+        return self.__dict__
